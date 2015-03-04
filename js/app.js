668f9ef5168810f3b6becde24e59c722ee3c0478
Sam Fentress <sfentress@concord.org>
2015-03-04 13:20:40 -0500

    publish: Merge pull request #4 from dougmartin/add-calculator
    generated from commit 68c80abee850fb85d4fd6293af77371dc350153e

--- js/app.js
+++ js/app.js
@@ -72,7 +72,7 @@ loadActivity(activityName);
 
 
 
-},{"./config":2,"./controllers/log":3,"./controllers/user":4,"./data/workbenchAdaptor":5,"./data/workbenchFBConnector":6,"./views/page.jsx":9}],2:[function(require,module,exports){
+},{"./config":2,"./controllers/log":3,"./controllers/user":4,"./data/workbenchAdaptor":5,"./data/workbenchFBConnector":6,"./views/page.jsx":10}],2:[function(require,module,exports){
 module.exports = {
   modelsBase: "activities/"
 }
@@ -113,6 +113,8 @@ var logManagerUrl = 'http://teaching-teamwork-log-manager.herokuapp.com/api/logs
       }
     },
 
+    // value: simple value (string, boolean, number)
+    // parameters: object
     logEvent = function(eventName, value, parameters) {
       var data = {
         application: "Teaching Teamwork",
@@ -343,7 +345,7 @@ module.exports = {
 }
 
 
-},{"../views/userRegistration.jsx":10,"./log":3}],5:[function(require,module,exports){
+},{"../views/userRegistration.jsx":11,"./log":3}],5:[function(require,module,exports){
 /**
  The workbench adaptor takes a TT-workbench definition such as
 
@@ -548,6 +550,270 @@ module.exports = WorkbenchFBConnector;
 
 
 },{}],7:[function(require,module,exports){
+// adapted from http://thecodeplayer.com/walkthrough/javascript-css3-calculator
+
+var logController = require('../controllers/log');
+    
+module.exports = CalculatorView = React.createClass({displayName: "CalculatorView",
+  
+  getInitialState: function() {
+    this.backspace = String.fromCharCode(8592);
+    this.inverse = '1/x';
+    this.squareRoot = String.fromCharCode(8730);
+    this.equals = '=';
+    this.plusMinus = String.fromCharCode(177);
+    
+    return {
+      input: '',
+      open: false,
+      evaled: false,
+      error: false,
+      closeRight: 10,
+      closeTop: 10,
+      openRight: 10,
+      openTop: 10
+    };
+  },
+  
+  open: function (e) {
+    logController.logEvent("Opened calculator");
+    this.setState({open: true});
+    e.preventDefault();    
+  },
+
+  close: function (e) {
+    logController.logEvent("Closed calculator");
+    this.setState({open: false});
+    e.preventDefault();    
+  },
+  
+  clear: function (e) {
+    logController.logEvent("Cleared calculator");
+    this.setState({
+      input: '',
+      evaled: false,
+      error: false
+    });
+    e.preventDefault();    
+  },
+
+  eval: function (e) {
+    var input = this.state.input,
+        equation = input.replace(/(\+|\-|\*|\/|\.)$/, ''),
+        key = e.target.innerHTML,
+        error = false,
+        evaled;
+        
+    if (equation) {
+      if (key === this.inverse) {
+        equation = "1/(" + equation + ")";
+      }
+      else if (key === this.squareRoot) {
+        equation = "Math.sqrt(" + equation + ")";
+      }
+      try {
+        evaled = eval(equation);
+        error = isNaN(evaled) || !isFinite(evaled);
+        if (!error) {
+          input = evaled.toString();
+        }
+        logController.logEvent("Calculation performed", null, {
+          "key": key,
+          "calculation": equation,
+          "result": evaled.toString()
+        });
+      }
+      catch (e) {
+        logController.logEvent("Calculation error", null, {
+          "key": key,
+          "calculation": equation,
+          "error": e.toString()
+        });
+        error = true;
+      }
+      this.setState({
+        input: input,
+        evaled: !error,
+        error: error
+      });
+    }
+    
+    e.stopPropagation();
+    e.preventDefault();    
+  },
+  
+  keyPressed: function (e) {
+    var input = this.state.input,
+        preInput = input,
+        empty = input.length === 0,
+        endsWithOperator = input.match(/(\+|\-|\*|\/)$/),
+        key = e.target.innerHTML,
+        evaled = false;
+    
+    // ignore clicks off the buttons
+    if (e.target.nodeName !== 'SPAN') {
+      return;
+    }
+    
+    if (key.match(/(\+|\-|\*|\/)/)) {
+      if (!empty) {
+        if (!endsWithOperator || key == '-') {
+          input += key;
+        }
+        else if (input.length > 1) {
+          input = input.replace(/.$/, key);
+        }
+      }
+      else if (empty && key == '-') {
+        input += key;
+      }
+    }
+    else if (key == '.') {
+      if (!input.match(/\./g) && !this.state.evaled) {
+        input += key;
+      }
+    }
+    else if (key === this.backspace) {
+      if (!empty && !this.state.error) {
+        input = input.substr(0, input.length - 1);
+      }
+    }
+    else if (key === this.plusMinus) {
+      if (input.match(/^-/)) {
+        input = input.replace(/^-/, '');
+      }
+      else {
+        input = '-' + input;
+      }
+      evaled = this.state.evaled;
+    }
+    else if (this.state.evaled) {
+      input = key;
+    }
+    else {
+      input += key;
+    }
+    
+    logController.logEvent("Calculator button pressed", null, {
+      "button": key,
+      "preCalculation": preInput,
+      "postCalculation": input,
+      "changed": this.state.input != input
+    });    
+
+    if (this.state.input != input) {
+      this.setState({
+        input: input,
+        evaled: evaled
+      });
+    }
+    
+    e.preventDefault();    
+  },
+  
+  startDrag: function (e) {
+    this.dragging = (this.state.open && (e.target.nodeName != 'SPAN'));
+    this.dragged = false;
+    if (!this.dragging) {
+      return;
+    }
+    this.startCalculatorPos = {
+      right: this.state.openRight,
+      top: this.state.openTop,
+    };
+    this.startMousePos = {
+      x: e.clientX,
+      y: e.clientY
+    };
+  },
+  
+  drag: function (e) {
+    var newPos;
+    if (this.dragging) {
+      // the calculations are reversed here only because we are setting the right pos and not the left
+      newPos = { 
+        openRight: this.startCalculatorPos.right + (this.startMousePos.x - e.clientX),
+        openTop: this.startCalculatorPos.top + (e.clientY - this.startMousePos.y)
+      };
+      if ((newPos.openRight != this.state.openRight) || (newPos.openTop != this.state.openTop)) {
+        this.setState(newPos);
+        this.dragged = true;
+      }
+    }
+  },
+  
+  endDrag:  function (e) {
+    if (this.dragged) {
+      logController.logEvent("Calculator dragged", null, {
+        "startTop": this.startCalculatorPos.top,
+        "startRight": this.startCalculatorPos.right,
+        "endTop": this.state.openTop,
+        "endRight": this.state.openRight,
+      });
+      this.dragged = false;
+    }
+    this.dragging = false;
+  },
+
+  render: function() {
+    var style = {
+      top: this.state.open ? this.state.openTop : this.state.closeTop, 
+      right: this.state.open ? this.state.openRight : this.state.closeRight
+    };
+    
+    if (this.state.open) {
+      return (
+        React.createElement("div", {id: "calculator", onMouseDown:  this.startDrag, onMouseMove:  this.drag, onMouseUp:  this.endDrag, style: style }, 
+          React.createElement("div", {className: "top very-top"}, 
+            React.createElement("span", {className: "title"}, "Calculator"), 
+            React.createElement("span", {className: "close", onClick:  this.close}, "X")
+          ), 
+          
+          React.createElement("div", {className: "top"}, 
+            React.createElement("span", {className: "clear", onClick:  this.clear}, "C"), 
+            React.createElement("div", {className:  this.state.error ? 'screen screen-error' : 'screen'},  this.state.input)
+          ), 
+          
+          React.createElement("div", {className: "keys", onClick:  this.keyPressed}, 
+            React.createElement("span", null, "7"), 
+            React.createElement("span", null, "8"), 
+            React.createElement("span", null, "9"), 
+            React.createElement("span", {className: "operator"}, "+"), 
+            React.createElement("span", {className: "operator operator-right"}, this.backspace), 
+            
+            React.createElement("span", null, "4"), 
+            React.createElement("span", null, "5"), 
+            React.createElement("span", null, "6"), 
+            React.createElement("span", {className: "operator"}, "-"), 
+            React.createElement("span", {className: "eval eval-right", onClick:  this.eval}, this.inverse), 
+            
+            React.createElement("span", null, "1"), 
+            React.createElement("span", null, "2"), 
+            React.createElement("span", null, "3"), 
+            React.createElement("span", {className: "operator"}, "/"), 
+            React.createElement("span", {className: "eval eval-right", onClick:  this.eval}, this.squareRoot), 
+            
+            React.createElement("span", null, "0"), 
+            React.createElement("span", null, "."), 
+            React.createElement("span", {className: "operator"}, this.plusMinus), 
+            React.createElement("span", {className: "operator"}, "*"), 
+            React.createElement("span", {className: "eval eval-right", onClick:  this.eval}, this.equals)
+          )
+        )
+      );
+    }
+    else {
+      return (
+        React.createElement("div", {id: "open-calculator", onClick:  this.open, style: style }, 
+          "Calculator"
+        )
+      );
+    }
+  }
+});
+
+
+},{"../controllers/log":3}],8:[function(require,module,exports){
 require('./goalTable.jsx');
 
 var ReactTransitionGroup = React.addons.TransitionGroup;
@@ -645,7 +911,7 @@ module.exports = ChatView = React.createClass({displayName: "ChatView",
 });
 
 
-},{"../controllers/log":3,"../controllers/user":4,"./goalTable.jsx":8}],8:[function(require,module,exports){
+},{"../controllers/log":3,"../controllers/user":4,"./goalTable.jsx":9}],9:[function(require,module,exports){
 module.exports = GoalTable = React.createClass({displayName: "GoalTable",
   render: function() {
     var rows = this.props.goal.map(function(val, i) {
@@ -672,8 +938,9 @@ module.exports = GoalTable = React.createClass({displayName: "GoalTable",
 });
 
 
-},{}],9:[function(require,module,exports){
+},{}],10:[function(require,module,exports){
 require('./chat.jsx');
+require('./calculator.jsx');
 
 var config = require('../config');
 
@@ -702,14 +969,15 @@ module.exports = PageView = React.createClass({displayName: "PageView",
         React.createElement("h2", null, "Circuit ",  this.props.circuit), 
         React.createElement("div", {id: "breadboard-wrapper"}), 
         chat, 
-        React.createElement("div", {id: "image-wrapper"}, image )
+        React.createElement("div", {id: "image-wrapper"}, image ), 
+        React.createElement(CalculatorView, null)
       )
     );
   }
 });
 
 
-},{"../config":2,"./chat.jsx":7}],10:[function(require,module,exports){
+},{"../config":2,"./calculator.jsx":7,"./chat.jsx":8}],11:[function(require,module,exports){
 var userController;
 
 module.exports = UserRegistrationView = React.createClass({displayName: "UserRegistrationView",
