595b629e2df0fe6a7880674b4ccd4609bb158076
Doug Martin <doug@dougmart.in>
2015-03-17 14:03:01 -0400

    Added gulp task to verify activity json files
    Uses a combination of two new gulp plugins (gulp-jsonlint and gulp-if) in the copy-activities.js gulp task to narrow the stream to *.json files using gulp-if and to verify the syntax of the json files using gulp-jsonlint.
    
    After running the task I fixed a trailing comma in activities/test-oscope.json

--- gulp/tasks/copy-activities.js
+++ gulp/tasks/copy-activities.js
@@ -1,7 +1,11 @@
 var gulp        = require('gulp');
 var config      = require('../config').activities;
+var gulpif      = require('gulp-if');
+var jsonlint    = require("gulp-jsonlint");
 
 gulp.task('copy-activities', function(){
   gulp.src(config.src, { base: config.base })
-    .pipe(gulp.dest(config.dest));
+    .pipe(gulp.dest(config.dest))
+    .pipe(gulpif('*.json', jsonlint()))
+    .pipe(gulpif('*.json', jsonlint.reporter()))
 });
