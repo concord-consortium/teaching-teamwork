db17be4125a2ea8116ac3655febe8e75128b51df
Doug Martin <doug@dougmart.in>
2015-03-29 11:46:12 -0400

    Added sidebar chat

---
+++ app/src/views/sidebar-chat.jsx
@@ -0,0 +1,90 @@
+var userController = require('../controllers/user'),
+    logController = require('../controllers/log'),
+    ChatItems, ChatItem;
+
+module.exports = React.createClass({
+
+  displayName: 'SidebarChat',
+
+  getInitialState: function() {
+    return {items: [], text: ""};
+  },
+
+  componentWillMount: function() {
+    var self = this;
+    userController.onGroupRefCreation(function() {
+      self.firebaseRef = userController.getFirebaseGroupRef().child("chat");
+      self.firebaseRef.on("child_added", function(dataSnapshot) {
+        var items = self.state.items.slice(0);
+        items.push(dataSnapshot.val());
+        self.setState({
+          items: items
+        });
+      }.bind(self));
+    });
+  },
+
+  componentWillUnmount: function() {
+    this.firebaseRef.off();
+  },
+
+  handleSubmit: function(e) {
+    var input = this.refs.text.getDOMNode();
+    e.preventDefault();
+    this.firebaseRef.push({
+      user: userController.getUsername(),
+      message: input.value
+    });
+    input.value = '';
+    logController.logEvent("Sent message", this.state.text);
+    this.setState({text: ""});
+  },
+
+  render: function() {
+    return (
+      <div className="sidebar-chat">
+        <div className="sidebar-chat-input">
+          <ChatItems items={ this.state.items } />
+          <form onSubmit={ this.handleSubmit }>
+            <input ref="text" type="text" />
+            <button onClick={ this.handleSubmit }>Send</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+});
+
+ChatItems = React.createClass({
+  displayName: 'ChatItems',
+
+  componentDidUpdate: function (prevProps) {
+    console.log('componentDidUpdate ' + prevProps.items.length + ' / ' + this.props.items.length);
+    if (prevProps.items.length !== this.props.items.length) {
+      var items = this.refs.items ? this.refs.items.getDOMNode() : null;
+      if (items) {
+        items.scrollTop = items.scrollHeight;
+      }
+    }
+  },
+
+  render: function () {
+    var user = userController.getUsername();
+    return <div ref="items" className="sidebar-chat-items">
+      {this.props.items.map(function(item, i) {
+        return <ChatItem key={ i } item={ item } me={ item.user == user } />;
+      })}
+    </div>;
+  }
+});
+
+ChatItem = React.createClass({
+  displayName: 'ChatItem',
+
+  render: function () {
+    return <div className={ this.props.me ? 'chat-item chat-item-me' : 'chat-item chat-item-others' }>
+        <b>{ this.props.item.user }:</b> { this.props.item.message }
+      </div>;
+  }
+});
+
