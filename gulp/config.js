0cf59b27f769cfef5974589946f1f7d872bf3363
Doug Martin <doug@dougmart.in>
2015-10-21 16:14:05 -0400

    Added React to bower and fixed React script include for PIC demo

--- gulp/config.js
+++ gulp/config.js
@@ -29,7 +29,7 @@ module.exports = {
     dest: dest
   },
   vendor: {
-    src: [bower + '/breadboard/**/*', bower + '/codemirror/**/*', bower + '/jsonlint/**/*', bower + '/mathjs/**/*', bower + '/jquery.cookie/**/*', bower + '/jquery/**/*'],
+    src: [bower + '/breadboard/**/*', bower + '/codemirror/**/*', bower + '/jsonlint/**/*', bower + '/mathjs/**/*', bower + '/jquery.cookie/**/*', bower + '/jquery/**/*', bower + '/react/**/*'],
     base: bower + '/',
     dest: dest + '/vendor'
   }
