5a5373d7c0d6bea27c9ad0b0f948b06c7aba4130
Doug Martin <doug@zoopdoop.com>
2016-05-26 15:20:37 -0400

    Added colors to the ribbon cable

--- app/src/views/shared/ribbon.js
+++ app/src/views/shared/ribbon.js
@@ -8,12 +8,13 @@ module.exports = React.createClass({
   render: function () {
     var selectedConstants = this.props.constants.selectedConstants(false),
         wires = [],
+        colors = ['#800000', '#008000', '#008080', '#00FFFF', '#000080', '#0000FF', '#800080', '#FF0000'],
         hole, i;
 
     if (this.props.connector) {
       for (i = 0; i < this.props.connector.holes.length; i++) {
         hole = this.props.connector.holes[i];
-        wires.push(line({key: i, x1: hole.cx, y1: 0, x2: hole.cx, y2: this.props.constants.RIBBON_HEIGHT, strokeWidth: selectedConstants.WIRE_WIDTH, stroke: hole.color}));
+        wires.push(line({key: i, x1: hole.cx, y1: 0, x2: hole.cx, y2: this.props.constants.RIBBON_HEIGHT, strokeWidth: selectedConstants.WIRE_WIDTH, stroke: colors[i % colors.length]}));
       }
     }
     return div({style: {height: this.props.constants.RIBBON_HEIGHT}},
