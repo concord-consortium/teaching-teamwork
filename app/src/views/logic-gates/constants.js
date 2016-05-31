e530ffd5037f24366d5c6861369fd3c30a5fadaf
Doug Martin <doug@zoopdoop.com>
2016-05-31 11:50:31 -0400

    Set default view for logic gate activity to see own circuit and added edit circuit button [#120300815] [#120301163]

--- app/src/views/logic-gates/constants.js
+++ app/src/views/logic-gates/constants.js
@@ -11,7 +11,8 @@ module.exports = constants = {
   SELECTED_FILL: '#bbb',
   UNSELECTED_FILL: '#777',
 
-  selectedConstants: function (selected) {
+  // in the logic gate activity there is no difference in layout between selected and unselected activities but we need to maintain this shared function signature
+  selectedConstants: function () {
     var boardHeight = (constants.WORKSPACE_HEIGHT - (2 * constants.RIBBON_HEIGHT)) / 2,
         logicDrawerLayout = {
           x: workspaceWidth - logicDrawerWidth,
@@ -20,49 +21,25 @@ module.exports = constants = {
           height: boardHeight
         };
 
-    if (selected) {
-      return {
-        WIRE_WIDTH: 3,
-        FOO_WIRE_WIDTH: 1,
-        CONNECTOR_HOLE_DIAMETER: 15,
-        CONNECTOR_HOLE_MARGIN: 4,
-        BOARD_HEIGHT: boardHeight,
-        COMPONENT_WIDTH: boardHeight * 0.5,
-        COMPONENT_HEIGHT: boardHeight * 0.5,
-        COMPONENT_SPACING: boardHeight * 0.5,
-        PIC_FONT_SIZE: 12,
-        CHIP_LABEL_SIZE: 16,
-        BUTTON_FONT_SIZE: 16,
-        PIN_WIDTH: 13.72,
-        PIN_HEIGHT: 13.72,
-        PROBE_WIDTH: 150,
-        PROBE_NEEDLE_HEIGHT: 5,
-        PROBE_HEIGHT: 20,
-        PROBE_MARGIN: 10,
-        LOGIC_DRAWER_LAYOUT: logicDrawerLayout
-      };
-    }
-    else {
-      return {
-        WIRE_WIDTH: 2,
-        FOO_WIRE_WIDTH: 1,
-        CONNECTOR_HOLE_DIAMETER: 10,
-        CONNECTOR_HOLE_MARGIN: 3,
-        BOARD_HEIGHT: boardHeight,
-        COMPONENT_WIDTH: boardHeight * 0.5,
-        COMPONENT_HEIGHT: boardHeight * 0.5,
-        COMPONENT_SPACING: boardHeight * 0.5,
-        PIC_FONT_SIZE: 8,
-        CHIP_LABEL_SIZE: 12,
-        BUTTON_FONT_SIZE: 13,
-        PIN_WIDTH: 8.64,
-        PIN_HEIGHT: 8.64,
-        PROBE_WIDTH: 95,
-        PROBE_NEEDLE_HEIGHT: 3,
-        PROBE_HEIGHT: 12,
-        PROBE_MARGIN: 10,
-        LOGIC_DRAWER_LAYOUT: logicDrawerLayout
-      };
-    }
+    return {
+      WIRE_WIDTH: 3,
+      FOO_WIRE_WIDTH: 1,
+      CONNECTOR_HOLE_DIAMETER: 15,
+      CONNECTOR_HOLE_MARGIN: 4,
+      BOARD_HEIGHT: boardHeight,
+      COMPONENT_WIDTH: boardHeight * 0.5,
+      COMPONENT_HEIGHT: boardHeight * 0.5,
+      COMPONENT_SPACING: boardHeight * 0.5,
+      PIC_FONT_SIZE: 12,
+      CHIP_LABEL_SIZE: 16,
+      BUTTON_FONT_SIZE: 16,
+      PIN_WIDTH: 13.72,
+      PIN_HEIGHT: 13.72,
+      PROBE_WIDTH: 150,
+      PROBE_NEEDLE_HEIGHT: 5,
+      PROBE_HEIGHT: 20,
+      PROBE_MARGIN: 10,
+      LOGIC_DRAWER_LAYOUT: logicDrawerLayout
+    };
   }
 };
