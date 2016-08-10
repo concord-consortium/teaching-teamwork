bac2e0c7df461baab9694a1c13a12a443f0798df
Doug Martin <doug@zoopdoop.com>
2016-08-10 08:29:14 -0400

    Added missing chip names from chip map.
    I rearranged the file and neglected to save if before committing the initial file.

--- app/src/data/logic-gates/chip-names.js
+++ app/src/data/logic-gates/chip-names.js
@@ -1,7 +1,9 @@
 module.exports = {
-  '7408': 'Quad 2-Input AND',
-  '7432': 'Quad 2-Input OR',
-  '7486': 'Quad 2-Input XOR',
+  '7400': 'Quad 2-Input NAND',
+  '7402': 'Quad 2-Input NOR',
   '7404': 'Hex Inverter',
-  '7411': 'Tri 3-Input AND'
+  '7408': 'Quad 2-Input AND',
+  '7411': 'Tri 3-Input AND',
+  '7432': 'Quad 2-Input OR',
+  '7486': 'Quad 2-Input XOR'
 };
