17c15dd84bdbef171fb587558f3bf98b66c4206f
Doug Martin <doug@zoopdoop.com>
2017-11-07 10:24:50 -0500

    Remove tests
    The karma-browserify module broke as on of its dependencies disappeared.   Given there was only a single test file the tests were removed instead of updated.

--- test/phantomJSUserAgentShim.js
+++
@@ -1,5 +0,0 @@
-// RapahaelJS checks for Safari's userAgent and borks if it doesn't see what it expects
-
-var newNavigator = Object.create(window.navigator);
-newNavigator.userAgent = "Version/7.1.2 ";
-navigator = newNavigator;
