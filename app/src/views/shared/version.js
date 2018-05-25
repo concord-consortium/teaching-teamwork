9fdf05bd54288f8b834555e5f99999e8f5f01dfa
Doug Martin <doug@zoopdoop.com>
2018-05-25 10:12:26 -0400

    Removed build link to GitHub when running in LARA [#120136869]

--- app/src/views/shared/version.js
+++ app/src/views/shared/version.js
@@ -1,19 +1,30 @@
 // NOTE: the __TT* variables are replaced by the browserify gulp task
 
 var div = React.DOM.div,
-    a = React.DOM.a;
+    a = React.DOM.a,
+    loadedFromLara;
+
+try {
+  loadedFromLara = window.self !== window.top;
+} catch (e) {
+  loadedFromLara = true;
+}
 
 module.exports = React.createClass({
   displayName: 'Version',
 
-  render: function() {
-    var commitHash = '__TT_COMMIT_HASH__',
-        version = commitHash.substr(0, 8);
+  renderVersion: function (commitHash) {
+    var version = commitHash.substr(0, 8),
+        href = 'https://github.com/concord-consortium/teaching-teamwork/commit/' + commitHash;
+    return loadedFromLara ? version : a({href: href, target: '_blank', title: commitHash}, version);
+  },
 
+  render: function() {
+    var commitHash = '__TT_COMMIT_HASH__';
     return div({className: "version-wrapper"},
       div({className: 'version-info'},
         'Build ',
-        a({href: 'https://github.com/concord-consortium/teaching-teamwork/commit/' + commitHash, target: '_blank', title: commitHash}, version),
+        this.renderVersion(commitHash),
         ', built on __TT_BUILD_DATE__')
     );
   }
